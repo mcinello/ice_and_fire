7162957a218de7f61ac101d242301f7104193d64
Michelle Cinello <m.cinello@gmail.com>
2017-10-30 15:08:48 -0400

    Finished part 2

--- test/factories/dragons.rb
+++ test/factories/dragons.rb
@@ -1,5 +1,22 @@
 FactoryGirl.define do
   factory :dragon do
     name "Trogdor"
+    rider
+
+    trait :child do
+      age {(0..3).to_a.sample}
+    end
+
+    trait :young do
+      age {(4..18).to_a.sample}
+    end
+
+    trait :middle do
+      age {(19..414).to_a.sample}
+    end
+
+    trait :ancient do
+      age {(415..1000).to_a.sample}
+    end
   end
 end
